18795f9458401bf994510fc8d7e5d0e7c7b2db25
Jacopo <jmangia@me.com>
2017-10-16 14:28:48 -0700

    Python 2.7 Flask version

---
+++ pythontest/Flask-2.7/app.py
@@ -0,0 +1,24 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request
+import grequests
+
+app = Flask(__name__)
+
+@app.route('/language', methods=['GET'])
+def get_language():
+    return jsonify({'language': 'Python'})
+
+
+@app.route('/request', methods=['POST'])
+def create_request():
+    if not request.json or not 'url' in request.json:
+        abort(400)
+    url = request.json['url']
+    result = grequests.map([grequests.get(url)])
+    
+    print(result[0].content)
+    
+    return jsonify({'language': 'Python'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8070)
