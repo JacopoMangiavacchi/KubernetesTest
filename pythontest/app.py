01aaec8e1bba4ed1fdaea421a2ef4a7465ad5d7b
Jacopo <jmangia@me.com>
2017-10-16 12:44:25 -0700

    Add Json Rest API

--- pythontest/app.py
+++ pythontest/app.py
@@ -1,11 +1,18 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 
-@app.route('/language')
-def index():
-    return "Python"
+@app.route('/language', methods=['GET'])
+def get_language():
+    return jsonify({'language': 'Python'})
+
+
+@app.route('/request', methods=['POST'])
+def create_request():
+    if not request.json or not 'url' in request.json:
+        abort(400)
+    return jsonify({'language': 'Python'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8070)
