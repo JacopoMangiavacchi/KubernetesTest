17f7a847efe6af302c63d297b441b0d8d6f51af2
Jacopo <jmangia@me.com>
2017-10-19 06:41:05 -0700

    Added POST JSON unmarshal

--- gotest/src/gotest/test.go
+++ gotest/src/gotest/test.go
@@ -4,6 +4,8 @@ import (
     "encoding/json"
     "log"
     "net/http"
+    "io"
+    "io/ioutil"
 
     "github.com/gorilla/mux"
 )
@@ -19,8 +21,8 @@ type Request struct {
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/language", HandleLanguage)
-    router.HandleFunc("/request", HandleRequest)
+    router.HandleFunc("/language", HandleLanguage).Methods("GET")
+    router.HandleFunc("/request", HandleRequest).Methods("POST")
 
     log.Fatal(http.ListenAndServe(":8040", router))
 }
@@ -31,6 +33,25 @@ func HandleLanguage(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
+    var request Request
+    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if err != nil {
+        panic(err)
+    }
+    if err := r.Body.Close(); err != nil {
+        panic(err)
+    }
+    if err := json.Unmarshal(body, &request); err != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(422) // unprocessable entity
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    }
+
+    log.Printf(request.Url)
+
+
     language := Language{Language: "Go"}
     json.NewEncoder(w).Encode(language)
 }
