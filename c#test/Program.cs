15fd736cc0187680d979189f401e1ed1415f7447
Jacopo <jmangia@me.com>
2017-10-20 16:30:20 -0700

    Adding C# .Net Core

---
+++ c#test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
+
+
+namespace csharptest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:8030")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapGet("language", context => {
+                return context.Response.WriteAsync("C#");
+            });
+
+            routeBuilder.MapPost("request", context => {
+                return context.Response.WriteAsync("C#");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+        }
+    }}
