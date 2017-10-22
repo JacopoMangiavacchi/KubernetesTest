b45e107c974398df2efba5bf573538a4dc9502c4
Jacopo <jmangia@me.com>
2017-10-22 15:36:35 -0700

    Single Java file

--- javatest/src/main/java/javatest/Application.java
+++ javatest/src/main/java/javatest/Application.java
@@ -2,6 +2,52 @@ package javatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+
+@JsonDeserialize
+class Language {
+
+    private String language;
+
+    public Language() {};
+
+    public Language(String language) {
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
+
+@JsonDeserialize
+class Request {
+
+    private String url;
+
+    public Request() {};
+
+    public Request(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
 
 @SpringBootApplication
 public class Application {
@@ -12,3 +58,20 @@ public class Application {
         app.run(args);
     }
 }
+
+@RestController
+class LanguageController {
+
+    @RequestMapping("/language")
+    public Language language() {
+        return new Language("Java");
+    }
+    @RequestMapping(value="/request", method=RequestMethod.POST)
+    public Language request(@RequestBody Request request) {
+        RestTemplate restTemplate = new RestTemplate();
+        Language language = restTemplate.getForObject(request.getUrl(), Language.class);
+
+        return new Language(language.getLanguage());
+    }
+}
+
