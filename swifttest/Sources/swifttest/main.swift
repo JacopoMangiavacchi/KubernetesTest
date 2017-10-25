dd5b3e9be9c735bf4eee503702ecf0a0a21c16fa
Jacopo <jmangia@me.com>
2017-10-25 12:36:34 -0700

    Add better support for Codable protocol

--- swifttest/Sources/swifttest/main.swift
+++ swifttest/Sources/swifttest/main.swift
@@ -18,8 +18,44 @@ router.get("/language") { request, response, next in
     next()
 }
 
-router.all("/request", middleware: BodyParser())
 router.post("/request") { request, response, next in
+    do {
+        var data = Data()
+        if try request.read(into: &data) > 0 {
+            let requestData: Request = try JSONDecoder().decode(Request.self, from: data)
+            var req = URLRequest(url: URL(string: requestData.url)!)
+            req.httpMethod = "GET"
+            
+            let dataTask = URLSession.shared.dataTask(with: req, completionHandler: { (data, getResponse, error) in
+                guard let data = data, error == nil else {
+                    print("Error: \(String(describing: error?.localizedDescription))")
+                    return
+                }
+                
+                if let httpStatus = getResponse as? HTTPURLResponse {
+                    if httpStatus.statusCode == 200 {
+                        do {
+                            try response.status(.OK).send(data: data).end()
+                            // let language = try JSONDecoder().decode(Language.self, from: data)
+                            // try response.status(.OK).send(data: JSONEncoder().encode(language)).end()
+                        }
+                        catch {
+                        }
+                    }
+                }
+
+                next()
+            })
+            
+            dataTask.resume()
+        }
+    }
+    catch {
+    }
+}
+
+router.all("/requestParser", middleware: BodyParser())
+router.post("/requestParser") { request, response, next in
     guard let parsedBody = request.body else {
         next()
         return
@@ -59,6 +95,26 @@ router.post("/request") { request, response, next in
         break
     }
 }
+
+struct Name: Codable {
+   let name: String
+}
+
+router.post("/") { request, response, next in
+    do {
+        var data = Data()
+        if try request.read(into: &data) > 0 {
+            let name: Name = try JSONDecoder().decode(Name.self, from: data)
+            try response.status(.OK).send(data: JSONEncoder().encode(name)).end()
+        }
+    }
+    catch {
+    }
+}
+
+
+
+
 
 Kitura.addHTTPServer(onPort: 8080, with: router)
 
