39af744b03b868136c8038eb0e7b781a883a8f78
Jacopo <jmangia@me.com>
2017-10-30 18:28:55 -0700

    Added Kitura 2.0 support

--- swifttest/Kitura2/Sources/swifttest/main.swift
+++ swifttest/Kitura2/Sources/swifttest/main.swift
@@ -17,12 +17,14 @@ func languageHandler(completion: ([Language]?, RequestError?) -> Void ) -> Void 
 }
 
 func requestHandler(request: Request, completion: @escaping (Language?, RequestError?) -> Void ) -> Void {
-    guard let client = KituraKit(baseURL: "http://localhost:8080") else { return }
+    if let url = URL(string: request.url), let scheme = url.scheme, let host = url.host, let port = url.port  {
+        guard let client = KituraKit(baseURL: "\(scheme)://\(host):\(port)") else { return }
 
-    client.get("/language") { (languages: [Language]?, error: Error?) in
-        guard error == nil else { return }
-        guard let languages = languages, languages.count > 0 else { return }
-        completion(languages[0], nil)
+        client.get(url.path) { (languages: [Language]?, error: Error?) in
+            guard error == nil else { return }
+            guard let languages = languages, languages.count > 0 else { return }
+            completion(languages[0], nil)
+        }
     }
 }
 
